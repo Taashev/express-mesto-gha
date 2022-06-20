c3e05b75a8bf16a8d4a8ac93e0bfa5d78b5d7332
Arsen Taashev <taashev.a.r92@yandex.ru>
2022-06-20 20:04:26 +0300

    ref: doesExist files

--- modules/doesCardExist.js
+++ modules/doesCardExist.js
@@ -1,11 +1,15 @@
 const Card = require('../models/card');
+const { checkError } = require('./checkError');
+const { NotFoundError } = require('../components/NotFoundError');
+
+const notFoundCard = new NotFoundError('Такой карточки нет');
 
 const doesCardExist = (req, res, next) => {
   const { cardId } = req.params;
 
   Card.findById(cardId, (err, card) => {
     if (card === null) {
-      res.send({ message: 'Такой карточки нет' });
+      next(checkError(notFoundCard));
       return;
     }
 
--- modules/doesUserExist.js
+++ modules/doesUserExist.js
@@ -1,11 +1,15 @@
 const User = require('../models/user');
+const { checkError } = require('./checkError');
+const { NotFoundError } = require('../components/NotFoundError');
+
+const notFoundUser = new NotFoundError('Такого пользователя не существует');
 
 const doesUserExist = (req, res, next) => {
   const { userId } = req.params;
 
   User.findById(userId, (err, user) => {
     if (user === null) {
-      res.send({ message: 'Такого пользователя не существует' });
+      next(checkError(notFoundUser));
       return;
     }
 
